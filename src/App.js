8f581b6b9749006659f7d0da98371e148f63ada8
Alexandre Pokhodoun <alexpokho@gmail.com>
2022-08-09 15:34:29 -0400

    added navigation

--- src/App.js
+++ src/App.js
@@ -7,35 +7,75 @@ import ResumeCard from './components/resume';
 import { Box } from '@mui/system';
 import Grid from '@mui/material/Grid';
 import Container from '@mui/material/Container';
-
+import { useRef } from 'react';
 
 function App() {
+
+  const aboutme = useRef()
+  const projects = useRef()
+  const hobbies = useRef()
+  const resume = useRef()
+
+  const navItems = [
+    {
+      name: 'About me',
+      ref: aboutme
+    },
+    {
+      name: 'Projects',
+      ref: projects
+    },
+    {
+      name: 'Hobbies',
+      ref: hobbies
+    },
+    {
+      name: 'Resume',
+      ref: resume
+    }
+  ]
+
+  function scrollTo(ref) {
+    if (ref && ref.current) {
+      setTimeout(function () {
+        let targetEle = ref.current
+        let pos = targetEle.style.position;
+        let top = targetEle.style.top;
+        targetEle.style.position = 'relative';
+        targetEle.style.top = '-64px';
+        targetEle.scrollIntoView({ behavior: 'smooth', block: 'start' })
+        targetEle.style.top = top;
+        targetEle.style.position = pos;
+      }, 1)
+    }
+  }
+
   return (
     <div className="App">
-      <ResponsiveAppBar></ResponsiveAppBar>
+      <ResponsiveAppBar data={navItems} scrollTo={scrollTo}></ResponsiveAppBar>
       <Container maxWidth="xl">
-      <Box sx={{ p: 2 }}>
+        <Box sx={{ p: 2, mt: 7 }}>
 
-        <Grid container spacing={2}>
-          <Grid item xs={12} md={3}>
-            <br></br>
-          <PictureCard></PictureCard>
+          <Grid container spacing={2}>
+            <Grid item xs={12} md={3}>
+              <br></br>
+              <PictureCard></PictureCard>
+            </Grid>
+            <Grid ref={aboutme} item xs={12} md={9}>
+              <AboutmeCard></AboutmeCard>
+            </Grid>
+            <Grid ref={projects} item xs={12}>
+              <ProjectsCard></ProjectsCard>
+            </Grid>
+            <Grid ref={hobbies} item xs={12}>
+              <HobbiesCard></HobbiesCard>
+            </Grid>
+            <Grid ref={resume} item xs={12}>
+              <ResumeCard></ResumeCard>
+            </Grid>
           </Grid>
-          <Grid item xs={12} md={9}>
-          <AboutmeCard></AboutmeCard>
-          </Grid>
-          <Grid item xs={12}>
-          <ProjectsCard></ProjectsCard>
-          </Grid>
-          <Grid item xs={12}>
-          <HobbiesCard></HobbiesCard>
-          </Grid>
-          <Grid item xs={12}>
-          <ResumeCard></ResumeCard>
-          </Grid>
-        </Grid>
 
-      </Box>
+        </Box>
       </Container>
 
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,21 +4,85 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+
+const drawerWidth = 150;
+//const navItems = ['About me', 'Projects', 'Hobbies', 'Resume'];
 
 export default function ButtonAppBar(props) {
-  return (
 
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const navItems = props.data
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.name} disablePadding>
+            <ListItemButton onClick={() => props.scrollTo(item.ref)} sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="fixed">
         <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
 
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Alexandre Pokhodoun
+            Alex Pokhodoun
           </Typography>
           <Button href="https://github.com/sashalex007" target="_blank" color="inherit">Github</Button>
           <Button href="https://www.linkedin.com/in/alexandrepokhodoun/" target="_blank" color="inherit">LinkedIn</Button>
         </Toolbar>
       </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block'},
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
     </Box>
   );
 }
