988d044a1c8225a533401b30e850b63a0a883f18
Alexandre Pokhodoun <alexpokho@gmail.com>
2022-08-09 16:29:34 -0400

    cleanup

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import { Container } from '@mui/system';
-import { Button } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 
 
 export default function ContactCard() {
-
 
     return (
 
--- src/components/resume.js
+++ src/components/resume.js
@@ -34,6 +34,7 @@ export default function ResumeCard() {
                 }}
             ></iframe>
             </Card>
+            <br></br>
         </Container>
         
 
