61e8cf50370b20e018780fc2b9abafe7cdd23fc8
Alexandre Pokhodoun <alexpokho@gmail.com>
2022-08-09 10:18:32 -0400

    more cleaning

--- src/components/hobbies.js
+++ src/components/hobbies.js
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import HobbiesTemplateCard from './hobbiesTemplate';
 import Grid from '@mui/material/Grid';
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import ProjectTemplateCard from './projectTemplate';
 import Grid from '@mui/material/Grid';
