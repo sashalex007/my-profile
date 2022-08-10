0c4487ee16b54be30c6f9099775a42e4f166ef65
Alexandre Pokhodoun <alexpokho@gmail.com>
2022-08-10 15:52:56 -0400

    cname

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 
 
 export default function AboutmeCard() {
