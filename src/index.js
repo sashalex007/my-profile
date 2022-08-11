8f8ea655d13e20850f428e4a27f21176320c3924
Alexandre Pokhodoun <alexpokho@gmail.com>
2022-08-10 22:46:52 -0400

    cleanup

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <CssBaseline />
+    <CssBaseline/>
     <App />
     </ThemeProvider>
   </React.StrictMode>
