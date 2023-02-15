cb471b3ce8c20f88db923207beadbdff5a1a2ef9
Alexandre Pokhodoun <alexpokho@gmail.com>
2023-02-15 13:36:53 -0500

    resume edit

--- src/components/resume.js
+++ src/components/resume.js
@@ -20,7 +20,7 @@ export default function ResumeCard() {
             <Card elevation={5} >
 
             <iframe
-                src="https://drive.google.com/file/d/1IgtE3o_6LlhwIg0uB1rIXvafgxWk4VAl/preview"
+                src="https://drive.google.com/file/d/1VjV8aF0Znt6jazCy6mdS29hOBB1giRwS/preview"
                 frameBorder="0"
                 scrolling="auto"
                 height="400"
